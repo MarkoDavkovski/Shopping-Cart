c6e1bf8722509f4f36ddbd34ebc02b1efa5e5954
MarkoDavkovski <marko.davkovski@gmail.com>
2023-09-19 20:03:32 +0200

    functionality improvements

--- src/components/AddToCartButton/AddToCartButton.jsx
+++ src/components/AddToCartButton/AddToCartButton.jsx
@@ -10,6 +10,82 @@ import { useHandleAddProduct } from "../../hooks/useHandleAddProduct";
 import { ProductsContext, CartContext } from "../../ProductsContext.jsx";
 import "./AddToCartButton.css";
 
+// const AddToCartButton = ({ productID }) => {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [quantityToAdd, setQuantityToAdd] = useState("");
+//   const { products, setProducts } = useContext(ProductsContext);
+//   const { cartProducts, setCartProducts } = useContext(CartContext);
+
+//   const handleAddProduct1 = useHandleAddProduct();
+
+//   const openModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   const handleQuantityChange = (event) => {
+//     const newQuantity = parseInt(event.target.value, 10);
+//     if (!isNaN(newQuantity) && newQuantity >= 0) {
+//       setQuantityToAdd(newQuantity);
+//     }
+//   };
+
+//   const handleAddToCart = () => {
+//     const product = products.find((p) => p.id === productID);
+//     const currentQuantityToAdd = parseInt(quantityToAdd, 10);
+//     if (isNaN(currentQuantityToAdd) || currentQuantityToAdd <= 0) {
+//       console.log(quantityToAdd);
+//       return alert("Please enter a valid quantity greater than 0.");
+//     }
+//     if (product.stock < currentQuantityToAdd) {
+//       alert("There are not enough products in store");
+//       closeModal();
+//     } else {
+//       handleAddProduct1(product, currentQuantityToAdd);
+//       closeModal();
+//       setQuantityToAdd("");
+//     }
+//   };
+
+//   return (
+//     <div className="add-to-cart-container">
+//       <button className="add-to-cart-btn" onClick={openModal}>
+//         <FontAwesomeIcon icon={faCartPlus} />
+//       </button>
+//       {isModalOpen && (
+//         <div className="modal active">
+//           <div className="modal-content">
+//             <h2>Add to Cart</h2>
+//             <input
+//               min={1}
+//               type="text"
+//               value={quantityToAdd}
+//               onChange={handleQuantityChange}
+//             />
+//             <div>
+//               <button onClick={closeModal}>Cancel</button>
+//               <button
+//                 onClick={handleAddToCart}
+//                 disabled={
+//                   product.stock === 0 ||
+//                   quantityToAdd === "" ||
+//                   quantityToAdd <= 0 ||
+//                   quantityToAdd > product.stock
+//                 }
+//               >
+//                 Add to Cart
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
 const AddToCartButton = ({ productID }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [quantityToAdd, setQuantityToAdd] = useState("");
@@ -17,6 +93,8 @@ const AddToCartButton = ({ productID }) => {
   const { cartProducts, setCartProducts } = useContext(CartContext);
 
   const handleAddProduct1 = useHandleAddProduct();
+
+  const product = products.find((p) => p.id === productID);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -34,13 +112,16 @@ const AddToCartButton = ({ productID }) => {
   };
 
   const handleAddToCart = () => {
-    const product = products.find((p) => p.id === productID);
+    if (!product) {
+      // Handle the case where the product is not found
+      return;
+    }
     const currentQuantityToAdd = parseInt(quantityToAdd, 10);
     if (isNaN(currentQuantityToAdd) || currentQuantityToAdd <= 0) {
       console.log(quantityToAdd);
       return alert("Please enter a valid quantity greater than 0.");
     }
-    if (product.stock < currentQuantityToAdd) {
+    if (product.stock === 0 || currentQuantityToAdd > product.stock) {
       alert("There are not enough products in store");
       closeModal();
     } else {
@@ -52,7 +133,11 @@ const AddToCartButton = ({ productID }) => {
 
   return (
     <div className="add-to-cart-container">
-      <button className="add-to-cart-btn" onClick={openModal}>
+      <button
+        className="add-to-cart-btn"
+        onClick={openModal}
+        disabled={product.stock === 0}
+      >
         <FontAwesomeIcon icon={faCartPlus} />
       </button>
       {isModalOpen && (
@@ -67,7 +152,17 @@ const AddToCartButton = ({ productID }) => {
             />
             <div>
               <button onClick={closeModal}>Cancel</button>
-              <button onClick={handleAddToCart}>Add to Cart</button>
+              <button
+                onClick={handleAddToCart}
+                disabled={
+                  product.stock === 0 ||
+                  quantityToAdd === "" ||
+                  quantityToAdd <= 0 ||
+                  quantityToAdd > product.stock
+                }
+              >
+                Add to Cart
+              </button>
             </div>
           </div>
         </div>
